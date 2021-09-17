b62b3ceb8ed13e2982c5067c5ec2f2cf03b3869e
Artem Schirov <artemschirov@gmail.com>
2021-09-18 00:05:09 +0300

    update Cell.dump()

--- cell.py
+++ cell.py
@@ -12,16 +12,10 @@ class Cell:
         self.y = y
         self.status = False
         self.neighbors_count = 0
-        # self.color = random.choice([COLORS])
+        # self.color = random.randint
 
 
     def dump(self):
-        # print(f"x: {self.x}, y: {self.y}, status: {self.status}, neighbors_count: {self.neighbors_count}")
         result = {"x": self.x, "y": self.y, "status": self.status, "neighbors_count": self.neighbors_count}
-        # print("return result", result)
         return result
 
-# c1 = Cell(1, 4)
-# c1.dump()
-# c2 = Cell(23, 47)
-# c1.dump()
