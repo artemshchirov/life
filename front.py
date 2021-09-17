50224aa9c37df0bd93d382c06e57dbd270c4e5de
Artem Schirov <artemschirov@gmail.com>
2021-09-18 00:06:08 +0300

    add fullscreen and resizable pywindow

--- front.py
+++ front.py
@@ -14,17 +14,46 @@ class GameWindow:
     def __init__(self):
         pygame.init()
         self.font_pygame = pygame.font.SysFont('Arial', FONT_SIZE)
-        self.surface = pygame.display.set_mode(
-            (self.WIDTH, self.HEIGHT))
+        self.surface = pygame.display.set_mode((self.WIDTH, self.HEIGHT), pygame.RESIZABLE)
         self.clock = pygame.time.Clock()
-
-    
-    def blit_surface(self, surface, x, y):
-        self.surface.blit(surface, (x, y))
 
     
     def display_update(self):
         pygame.display.update()
+
+
+    def check_events(self):
+        for event in pygame.event.get():
+
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_SPACE:
+                    return ['HOTKEY', (0, 0), 'SPACE']
+                if event.key == K_e:
+                    return['EMPTY_CELLS']
+
+            if event.type == pygame.VIDEORESIZE:
+                return['FULLSCREEN', (event.w, event.h)]
+
+            elif event.type == MOUSEBUTTONDOWN:
+
+                if event.button == 1:  
+                    if event.pos[0] in range(config.FIELD_WIDTH) and event.pos[1] in range(config.FIELD_HEIGHT):
+                        pos = (event.pos[0] // self.CELL_SIZE, event.pos[1] // self.CELL_SIZE)
+                        return ['NEIGHBORS_UPDATE', pos]
+                    return['LMC', event.pos]  # Left Mouse Click
+                elif event.button == 2:  
+                    return['SWC', event.pos]  # Scroll Wheel Press
+                elif event.button == 3:
+                    return['RMC', event.pos]
+                elif event.button == 4:
+                    return['SWU', event.pos]
+                elif event.button == 5:
+                    return['LMC', event.pos]              
+
+        return 0
 
 
 
@@ -35,12 +64,9 @@ class Field:
     CELL_SIZE = config.CELL_SIZE
     COLOR_LIVE = config.COLOR_LIVE
     COLOR_DEAD = config.COLOR_DEAD
-    play = False
 
     def __init__(self):
-        # pygame.init()
         self.font_pygame = pygame.font.SysFont('Arial', FONT_SIZE)
-        # self.clock = pygame.time.Clock()
         self.surface = pygame.Surface((self.WIDTH, self.HEIGHT))
 
 
@@ -55,6 +81,14 @@ class Field:
                      self.CELL_SIZE,
                      self.CELL_SIZE),
                      config.EMPTY_CELLS)
+                # pygame.draw.circle(
+                #     self.surface, 
+                #     self.COLOR_LIVE, 
+                #     (cells[i].x * self.CELL_SIZE,
+                #      cells[i].y * self.CELL_SIZE),
+                #      self.CELL_SIZE // 2,
+
+                #      config.EMPTY_CELLS)
                 
 
     def draw_lines(self):
@@ -64,57 +98,25 @@ class Field:
                 config.COLOR_LINE,
                 (0, x),
                 (self.WIDTH, x))
-
             if x % (self.CELL_SIZE * 2)  == 0:  # 2 lines next to each other = bold line
                 pygame.draw.line(
                     self.surface, 
                     config.COLOR_LINE,
                     (0, x+1),
                     (self.WIDTH, x+1))
-
+    
         for y in range(0, self.WIDTH, self.CELL_SIZE):
             pygame.draw.line(
                 self.surface, 
                 config.COLOR_LINE, 
                 (y, 0),
                 (y, self.HEIGHT))
-
             if y % (self.CELL_SIZE * 2)  == 0:  
                 pygame.draw.line(
                     self.surface, 
                     config.COLOR_LINE,
                     (y+1, 0),
                     (y+1, self.HEIGHT))
-
-
-    def blit_surface(self, surface, x, y):
-        self.surface.blit(surface, (x, y))
-
-
-    def check_events(self):
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == KEYDOWN:
-                if event.key == K_SPACE:
-                    return ['PLAY_UPDATE']
-                if event.key == K_e:
-                    return['EMPTY_CELLS']
-            elif event.type == MOUSEBUTTONDOWN:
-                x, y = pygame.mouse.get_pos()
-
-                if pygame.mouse.get_pressed()[0]:
-                    return['BUTTON', pygame.mouse.get_pos()]
-
-                if x in range(config.FIELD_WIDTH) and y in range(config.FIELD_HEIGHT):
-                    pos = (x // self.CELL_SIZE, y // self.CELL_SIZE)
-                    return ['NEIGHBORS_UPDATE', pos]
-
-
-            # button1.click(event)
-
-        return ['']
 
 
 
@@ -126,10 +128,9 @@ class NavigationBar:
     COLOR_BACKGROUND = config.COLOR_NAVBAR
 
     def __init__(self):
-        # pygame.init()
         self.font_pygame = pygame.font.SysFont('Arial', FONT_SIZE)
-        # self.clock = pygame.time.Clock()
         self.surface = pygame.Surface((self.WIDTH, self.HEIGHT))
+        self.clock = pygame.time.Clock()
 
 
     def draw_info(self, generation, live_cells, cells):
@@ -142,6 +143,13 @@ class NavigationBar:
         text_count_cells1 = self.font_pygame.render(
             "DEAD CELLS: " + str(abs(len(cells)-live_cells)), 1, self.COLOR_INFO)
 
+        text_scale = self.font_pygame.render(
+            "SCALE", 1, self.COLOR_INFO)
+        text_speed = self.font_pygame.render(
+            "FPS", 1, self.COLOR_INFO)
+        self.surface.blit(text_scale, (config.NAVBAR_WIDTH * 0.35, config.NAVBAR_HEIGHT - config.NAVBAR_HEIGHT * 0.77))
+        self.surface.blit(text_speed, (config.NAVBAR_WIDTH * 0.41, config.NAVBAR_HEIGHT - config.NAVBAR_HEIGHT * 0.67))
+
         left_space = int(config.NAVBAR_WIDTH * 0.05)
         top_space = int(config.NAVBAR_WIDTH * 0.05)
 
@@ -149,43 +157,38 @@ class NavigationBar:
         self.surface.blit(text_count_cells, (left_space, top_space + FONT_SIZE))
         self.surface.blit(text_count_cells1, (left_space, top_space + FONT_SIZE*2))
 
-    def blit_surface(self, surface, x, y):
-        self.surface.blit(surface, (x, y))
+
+    def blit_surfaces(self, surfaces):
+        for surface_info in surfaces:
+            surface = surface_info[0]
+            x = surface_info[1]
+            y = surface_info[2]
+            self.surface.blit(surface, (x, y))
+
+
 
 
 
 class Button():
     """Create a button, then blit the surface in the while loop"""
 
-    def __init__(self, text: str, pos: tuple, font: int, bg="black", feedback=""):
+    def __init__(self, text: str, pos: tuple, font: int, size=(config.BUTTON_WIDTH, config.BUTTON_HEIGHT), bg="black", feedback=""):
+        self.name = text
         self.x, self.y = pos
+        self.width, self.height = size
         self.font = pygame.font.SysFont("Arial", font)
         if feedback == "":
             self.feedback = "text"
         else:
             self.feedback = feedback
-        self.change_text(text, bg)
-
+        self.change_text(self.name, bg)
 
 
     def change_text(self, text, bg="black"):
-        """Change the text whe you click"""
-        print("PRESSED")
+        """Change the text when you click"""
         self.text = self.font.render(text, 1, pygame.Color("White"))
-        # self.size = self.text.get_size() 
-        self.surface = pygame.Surface((config.BUTTON_WIDTH, config.BUTTON_HEIGHT))
+        self.surface = pygame.Surface((self.width, self.height))
         self.surface.fill(bg)
-        self.surface.blit(self.text, (config.BUTTON_WIDTH // 2 - self.text.get_size()[0] // 2, config.BUTTON_HEIGHT // 2 - self.text.get_size()[1] // 2))
-        self.rect = pygame.Rect(config.FIELD_WIDTH + self.x, config.NAVBAR_HEIGHT - self.y, config.BUTTON_WIDTH, config.BUTTON_HEIGHT)
-        # print(f"Rect {self.text} coords: {config.FIELD_WIDTH + self.x, config.NAVBAR_HEIGHT - self.y}")
-    
-    # def show(self):
-    #     self.surface.blit(self.surface, (self.x, self.y))
-
-    # def click(self, event):
-    #     x, y = pygame.mouse.get_pos()
-    #     if event.type == pygame.MOUSEBUTTONDOWN:
-    #         if pygame.mouse.get_pressed()[0]:
-    #             if self.rect.collidepoint(x, y):
-    #                 self.change_text(self.feedback, bg="red")
-
+        self.surface.blit(self.text, (self.width // 2 - self.text.get_size()[0] // 2, self.height // 2 - self.text.get_size()[1] // 2))
+        self.rect = pygame.Rect(config.FIELD_WIDTH + self.x, config.NAVBAR_HEIGHT - self.y, self.width, self.height)
+        
--- main.py
+++ main.py
@@ -1,35 +1,141 @@
-from front import Field
-import core
+import pygame
+from pygame.constants import FULLSCREEN
 import config
+import front
+import core
+import cell
 import datetime
 import random
+import sys
 
-FRONT = config.FRONT
-if FRONT:
-    import front
+
+if config.FRONT:
     WINDOW = front.GameWindow()
     FIELD = front.Field()
     NAVBAR = front.NavigationBar()
+
+    btn_add_cells = front.Button(
+        "ADD",
+        (config.NAVBAR_WIDTH * 0.74, 
+        config.NAVBAR_HEIGHT * 0.4),
+        size=(config.BUTTON_WIDTH // 1.8, config.BUTTON_HEIGHT),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="ADD")
+
+    btn_del_cells = front.Button(
+        "DEL",
+        (config.NAVBAR_WIDTH * 0.05, 
+        config.NAVBAR_HEIGHT * 0.4),
+        size=(config.BUTTON_WIDTH // 1.8, config.BUTTON_HEIGHT),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="DEL")
+
+    btn_scale_plus = front.Button(
+        "+",
+        (config.NAVBAR_WIDTH * 0.74, 
+        config.NAVBAR_HEIGHT * 0.8),
+        size=(config.BUTTON_WIDTH // 1.8, config.BUTTON_HEIGHT),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="+")
+
+    btn_scale_minus = front.Button(
+        "-",
+        (config.NAVBAR_WIDTH * 0.05, 
+        config.NAVBAR_HEIGHT * 0.8),
+        size=(config.BUTTON_WIDTH // 1.8, config.BUTTON_HEIGHT),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="-")
+
+    btn_speed_plus = front.Button(
+        "+",
+        (config.NAVBAR_WIDTH * 0.74, 
+        config.NAVBAR_HEIGHT * 0.7),
+        size=(config.BUTTON_WIDTH // 1.8, config.BUTTON_HEIGHT),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="+")
+
+    btn_speed_minus = front.Button(
+        "-",
+        (config.NAVBAR_WIDTH * 0.05, 
+        config.NAVBAR_HEIGHT * 0.7),
+        size=(config.BUTTON_WIDTH // 1.8, config.BUTTON_HEIGHT),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="-")
+
+    btn_reset = front.Button(
+        "RESET",
+        (config.NAVBAR_WIDTH * 0.3, 
+        config.NAVBAR_HEIGHT * 0.4),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="RESET")
+
+    btn_step = front.Button(
+        "STEP",
+        (config.NAVBAR_WIDTH * 0.5225, 
+        config.NAVBAR_HEIGHT * 0.5),
+        size=(config.NAVBAR_WIDTH * 0.4525,
+        config.NAVBAR_HEIGHT * 0.09),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="STEP")
+
+    btn_start = front.Button(
+        "START",
+        (config.NAVBAR_WIDTH * 0.05, 
+        config.NAVBAR_HEIGHT * 0.5),
+        size=(config.NAVBAR_WIDTH * 0.4525, config.NAVBAR_HEIGHT * 0.09),
+        font=config.BUTTON_FONT_SIZE,
+        bg="green4",
+        feedback="PAUSE")
+
+    btn_help = front.Button(
+        "HELP?",
+        (config.NAVBAR_WIDTH * 0.05, 
+        config.NAVBAR_HEIGHT * 0.2),
+        size=(config.NAVBAR_WIDTH * 0.4525,
+        config.NAVBAR_HEIGHT * 0.09),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="HELP?")
+
+    btn_fullscreen = front.Button(
+        "SCREEN",
+        (config.NAVBAR_WIDTH * 0.5225, 
+        config.NAVBAR_HEIGHT * 0.2),
+        size=(config.NAVBAR_WIDTH * 0.4525,
+        config.NAVBAR_HEIGHT * 0.09),
+        font=config.BUTTON_FONT_SIZE,
+        bg="black",
+        feedback="SCREEN")
 
     btn_options = front.Button(
         "OPTIONS",
         (config.NAVBAR_WIDTH * 0.05, 
         config.NAVBAR_HEIGHT * 0.1),
-        font=20,
+        size=(config.NAVBAR_WIDTH * 0.4525,
+        config.NAVBAR_HEIGHT * 0.09),
+        font=config.BUTTON_FONT_SIZE,
         bg="black",
         feedback="<- BACK")
 
     btn_exit = front.Button(
         "EXIT",
-        (config.NAVBAR_WIDTH * 0.55, 
+        (config.NAVBAR_WIDTH * 0.5225, 
         config.NAVBAR_HEIGHT * 0.1),
-        font=20,
+        size=(config.NAVBAR_WIDTH * 0.4525,
+        config.NAVBAR_HEIGHT * 0.09),
+        font=config.BUTTON_FONT_SIZE,
         bg="black",
         feedback="ok")
 
-# PLAY = not FRONT
-PLAY = True
-config.RECORD = True if not FRONT else config.RECORD
+config.RECORD = True if not config.FRONT else config.RECORD
 
 if config.RECORD:
     f = open('record.log', 'w')
@@ -51,19 +157,62 @@ def check_endgame():
             cell_auto.clean_cells()
             cell_auto.make_RANDOM_CELLS()
         elif ENDGAME == 2:  # add new cells
-            print('2')
-            FIELD.COLOR_LIVE = random.randint(100, 245), random.randint(100, 245), random.randint(100, 245)
+            FIELD.COLOR_LIVE = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
             cell_auto.make_RANDOM_CELLS()
         elif ENDGAME == 3:  # clean field
             cell_auto.clean_cells()
-        elif ENDGAME == 4:  # do nothing
+        elif ENDGAME == 0:  # do nothing
             pass
 
+def NAVBAR_surfaces_main():
+    surfaces = [
+    # GAME INFO
+
+    # BUTTONS INFO
+
+    # BUTTONS
+        [btn_options.surface, btn_options.x, config.NAVBAR_HEIGHT - btn_options.y],
+        [btn_exit.surface, btn_exit.x, config.NAVBAR_HEIGHT - btn_exit.y],
+        [btn_add_cells.surface,btn_add_cells.x,config.NAVBAR_HEIGHT - btn_add_cells.y],
+        [btn_del_cells.surface,btn_del_cells.x,config.NAVBAR_HEIGHT - btn_del_cells.y],
+        [btn_scale_plus.surface,btn_scale_plus.x,config.NAVBAR_HEIGHT - btn_scale_plus.y],
+        [btn_scale_minus.surface,btn_scale_minus.x,config.NAVBAR_HEIGHT - btn_scale_minus.y],
+        [btn_speed_plus.surface,btn_speed_plus.x,config.NAVBAR_HEIGHT - btn_speed_plus.y],
+        [btn_speed_minus.surface,btn_speed_minus.x,config.NAVBAR_HEIGHT - btn_speed_minus.y],
+        [btn_reset.surface,btn_reset.x,config.NAVBAR_HEIGHT - btn_reset.y],
+        [btn_step.surface,btn_step.x,config.NAVBAR_HEIGHT - btn_step.y],
+        [btn_start.surface,btn_start.x,config.NAVBAR_HEIGHT - btn_start.y],        
+        [btn_fullscreen.surface,btn_fullscreen.x,config.NAVBAR_HEIGHT - btn_fullscreen.y],
+        [btn_help.surface,btn_help.x,config.NAVBAR_HEIGHT - btn_help.y],
+        [btn_options.surface,btn_options.x,config.NAVBAR_HEIGHT - btn_options.y],
+    ]   
+    return surfaces
+
+def NAVBAR_surfaces_options():
+    """Return list with surfaces that displayed on sidebar when click on button OPTIONS"""
+    surfaces = [
+        [btn_options.surface, btn_options.x, config.NAVBAR_HEIGHT - btn_options.y],
+        [btn_exit.surface, btn_exit.x, config.NAVBAR_HEIGHT - btn_exit.y],
+        [btn_fullscreen.surface,btn_fullscreen.x,config.NAVBAR_HEIGHT - btn_fullscreen.y],
+        [btn_help.surface,btn_help.x,config.NAVBAR_HEIGHT - btn_help.y],
+    ]
+    return surfaces
+
+def get_screen_size():
+    """Get info about user screen width, height"""
+    pygame.init()
+    screen_info = pygame.display.Info()
+    screen_width = screen_info.current_w
+    screen_height = screen_info.current_h
+    return screen_width, screen_height
 
 def main_cycle():
-    global PLAY
-    finished = False
-    while not finished:
+
+    PLAY = False
+    options = False
+
+    running = True
+    while running:
         WINDOW.clock.tick(config.FPS)
         if PLAY:
             cell_auto.generation += 1
@@ -79,57 +228,130 @@ def main_cycle():
             delta_time = (end_time - start_time)
             write_log(f'{delta_time.seconds}.{delta_time.microseconds}')
 
-        if FRONT:
-            FIELD.surface.fill((FIELD.COLOR_DEAD))
+        if config.FRONT:
+
+            FIELD.surface.fill(FIELD.COLOR_DEAD)
             FIELD.draw_lines()
             FIELD.draw_cells(cell_auto.cells) 
             NAVBAR.draw_info(cell_auto.generation,
                         cell_auto.live_cells, cell_auto.cells)
+            if options:
+                NAVBAR.blit_surfaces(NAVBAR_surfaces_options())
+            else:
+                NAVBAR.blit_surfaces(NAVBAR_surfaces_main())
 
-        
-            NAVBAR.blit_surface(btn_options.surface, 
-                btn_options.x, 
-                config.NAVBAR_HEIGHT - btn_options.y)
-
-            NAVBAR.blit_surface(btn_exit.surface, 
-                btn_exit.x, 
-                config.NAVBAR_HEIGHT - btn_exit.y)
-
-
-            WINDOW.blit_surface(FIELD.surface, 0, 0)
-            WINDOW.blit_surface(NAVBAR.surface, FIELD.WIDTH, 0)
-
-        
+            WINDOW.surface.blit(FIELD.surface, (0, 0))
+            WINDOW.surface.blit(NAVBAR.surface, (FIELD.WIDTH, 0))
             WINDOW.display_update()
 
-            result = FIELD.check_events()
-            if result[0] == 'PLAY_UPDATE':
-                print(f'button "space" was pressed, result: {result}')
-                PLAY = not PLAY
+            result =  WINDOW.check_events()
+            if result:
+                print(result)
 
-            elif result[0] == 'NEIGHBORS_UPDATE':
-                cell_auto.cells[result[1][1] + result[1][0] * cell_auto.HEIGHT].status = True if not cell_auto.cells[result[1][1] +
-                                                                                                                    result[1][0] * cell_auto.HEIGHT].status else False
-                cell_auto.update_neighbors()
-            elif result[0] == 'EMPTY_CELLS':
-                print(f'button "e" was pressed, result: {result}')
-                config.EMPTY_CELLS = not config.EMPTY_CELLS
-            
-            elif result[0] == 'BUTTON':
+                if result[0] == 'FULLSCREEN':
+                    WINDOW.surface = pygame.display.set_mode((result[1][0], result[1][1]),
+                                pygame.RESIZABLE)
+
+                if result[0] == 'NEIGHBORS_UPDATE':
+                    cell_auto.cells[result[1][1] + result[1][0] * cell_auto.HEIGHT].status = True if not cell_auto.cells[result[1][1] +
+                                                                                                                        result[1][0] * cell_auto.HEIGHT].status else False
+                    cell_auto.update_neighbors()
+
+                elif result[0] == 'EMPTY_CELLS':
+                    config.EMPTY_CELLS = not config.EMPTY_CELLS
                 
-                print(f"btn_options here: {btn_options.rect.x, btn_options.rect.y}")
-                print(f"collidepoint check here: {result[1]}")    
-              
-                if btn_options.rect.collidepoint(result[1]):
-                    btn_options.change_text(btn_options.feedback, bg="red")
+                elif result[0] == 'LMC' or result[0] == 'HOTKEY':
 
-                if btn_exit.rect.collidepoint(result[1]):
-                    btn_exit.change_text(btn_exit.feedback, bg="red")
+                    if btn_start.rect.collidepoint(result[1]) or 'SPACE' in result:
+                        PLAY = not PLAY
+                        if PLAY:
+                            btn_start.change_text(btn_start.feedback, bg="darkorange3")
+                        else:
+                            btn_start.change_text(btn_start.name, bg="green4")
 
+                    elif btn_add_cells.rect.collidepoint(result[1]):
+                        cell_auto.make_RANDOM_CELLS()
+
+                    elif btn_del_cells.rect.collidepoint(result[1]):
+                        cell_auto.clean_cells()
+                        PLAY = False
+
+                    elif btn_scale_plus.rect.collidepoint(result[1]):
+                        PLAY = False
+                        FIELD.CELL_SIZE += 1
+                        cell_auto.CELL_SIZE += 1
+                        config.CELL_SIZE += 1
+                        cell_auto.WIDTH = config.FIELD_WIDTH // config.CELL_SIZE
+                        cell_auto.HEIGHT = config.FIELD_HEIGHT // config.CELL_SIZE
+                        cell_auto.cells = {
+                            j + cell_auto.HEIGHT * i: cell.Cell(i, j) for i in range(cell_auto.WIDTH) for j in range(cell_auto.HEIGHT)}
+                        cell_auto.make_RANDOM_CELLS()
+                        print(f"config.CELL_SIZE: {config.CELL_SIZE}")
+
+                    elif btn_scale_minus.rect.collidepoint(result[1]):
+                        PLAY = False
+                        FIELD.CELL_SIZE -= 1
+                        cell_auto.CELL_SIZE -= 1
+                        if config.CELL_SIZE > 1:
+                            config.CELL_SIZE -= 1
+                        else:
+                            config.CELL_SIZE = 1
+                        cell_auto.WIDTH = config.FIELD_WIDTH // config.CELL_SIZE
+                        cell_auto.HEIGHT = config.FIELD_HEIGHT // config.CELL_SIZE
+                        cell_auto.cells = {
+                            j + cell_auto.HEIGHT * i: cell.Cell(i, j) for i in range(cell_auto.WIDTH) for j in range(cell_auto.HEIGHT)}
+                        cell_auto.make_RANDOM_CELLS()
+                        print(f"config.CELL_SIZE: {config.CELL_SIZE}")
+
+                    elif btn_speed_plus.rect.collidepoint(result[1]):
+                        config.FPS += 2  
+                        print(f"FPS: {config.FPS}")
+
+                    elif btn_speed_minus.rect.collidepoint(result[1]):
+                        if config.FPS > 2:
+                            config.FPS -= 2
+                        else:
+                            config.FPS = 2
+                        print(f"FPS: {config.FPS}")
+
+                    elif btn_reset.rect.collidepoint(result[1]):
+                        FIELD.COLOR_LIVE = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
+                        cell_auto.clean_cells()
+                        cell_auto.make_RANDOM_CELLS()
+
+                    elif btn_step.rect.collidepoint(result[1]):
+                        PLAY = False
+                        cell_auto.generation += 1
+                        cell_auto.calculate_cells() 
+                        cell_auto.update_cells()
+                        cell_auto.update_neighbors()
+
+                    elif btn_help.rect.collidepoint(result[1]):
+                        pass
+
+                    elif btn_fullscreen.rect.collidepoint(result[1]):
+                        fullscreen = True
+                        
+                        WINDOW.surface = pygame.display.set_mode((0, 0),
+                                pygame.FULLSCREEN)
+
+                        btn_fullscreen.change_text(btn_fullscreen.feedback, bg="red")
+
+                    elif btn_options.rect.collidepoint(result[1]):
+                        options = not options
+                        if options:
+                            btn_options.change_text(btn_options.feedback, bg="grey40")
+                        else:
+                            btn_options.change_text(btn_options.feedback, bg="black")
+
+                    elif btn_exit.rect.collidepoint(result[1]):
+                        pygame.quit()
+                        sys.exit()
 
 
 
 def main():
+    print(f"FPS: {config.FPS}")
     main_cycle()
 
 
